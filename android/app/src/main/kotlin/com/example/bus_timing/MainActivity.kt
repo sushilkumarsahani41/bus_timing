e86b50ce0f2967bc742c373f0a014d1938a2091e
sushilkumarsahani41 <dev@sahanisushilkumar.in>
2022-12-25 01:57:20 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/bus_timing/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bus_timing
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/toCollege.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ToCollege extends StatelessWidget {
+  const ToCollege({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("To College"),
+      ),
+    );
+  }
+}
